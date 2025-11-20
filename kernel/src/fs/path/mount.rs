6ac787cdf06334606b3bcee464df96e7e66c9800
Chen Chengjun <chenchengjun.ccj@antgroup.com>
2025-11-20 09:18:46 +0000

    Fix some mount-related bugs

--- kernel/src/fs/path/mount.rs
+++ kernel/src/fs/path/mount.rs
@@ -192,7 +192,7 @@ impl Mount {
             let old_children = old_mount.children.read();
             for old_child_mount in old_children.values() {
                 let mountpoint = old_child_mount.mountpoint().unwrap();
-                if !mountpoint.is_descendant_of(old_mount.root_dentry()) {
+                if !mountpoint.is_equal_or_descendant_of(old_mount.root_dentry()) {
                     continue;
                 }
                 let new_child_mount =
