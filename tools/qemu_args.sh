73e5894b4de9281af0414ee79051aeef596de9c3
Wei Zhang <ruoyuan.zw@antgroup.com>
2025-12-09 15:31:47 +0800

    Set VGA resolution to 1920x1200

--- tools/qemu_args.sh
+++ tools/qemu_args.sh
@@ -104,6 +104,7 @@ fi
 
 QEMU_ARGS="\
     $COMMON_QEMU_ARGS \
+    -device VGA,xres=1920,yres=1200 \
     -machine q35,kernel-irqchip=split \
     -device virtio-blk-pci,bus=pcie.0,addr=0x6,drive=x0,serial=vext2,disable-legacy=on,disable-modern=off,queue-size=64,num-queues=1,request-merging=off,backend_defaults=off,discard=off,write-zeroes=off,event_idx=off,indirect_desc=off,queue_reset=off$IOMMU_DEV_EXTRA \
     -device virtio-blk-pci,bus=pcie.0,addr=0x7,drive=x1,serial=vexfat,disable-legacy=on,disable-modern=off,queue-size=64,num-queues=1,request-merging=off,backend_defaults=off,discard=off,write-zeroes=off,event_idx=off,indirect_desc=off,queue_reset=off$IOMMU_DEV_EXTRA \
