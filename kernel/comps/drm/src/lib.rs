d114bae373329a029e480938e81c02a307c48d81
Tina Zhang <tina.zhang.zw@outlook.com>
2025-12-14 10:20:53 +0800

    Add drm component (WIP

---
+++ kernel/comps/drm/src/lib.rs
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MPL-2.0
+
+//! The drm subsystem abstractions of Asterinas.
+#![no_std]
+#![deny(unsafe_code)]
+
+#[init_component]
+fn init() -> Result<(), ComponentInitError> {
+    Ok(())
+}
